ce0de281e61f6ee82de7f4234d195ef6843897bd
Ankur Rathi <ankurrathi1611@gmail.com>
2021-08-07 16:06:24 +0530

    First Commit

--- test.php
+++ test.php
@@ -1,7 +1,20 @@
-
-    <?php
-    if (isset($_GET['id']) == "5") {
-      // code...
-      echo "Yes its here";
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><?php
+    if ($_GET['id'] == "5") {
+      echo $_GET['id'];
+      echo "Yes it is 5";
+    }
+    elseif ($_GET['id'] == "4")  {
+    	echo $_GET['id'];
+      echo "It is 4";
     }
     ?>
+  </h1>
+  </body>
+</html>
