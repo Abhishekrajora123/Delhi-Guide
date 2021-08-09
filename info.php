61aa11da5cf20528a0d927f72ea20a361ea5fb30
Ankur Rathi <ankurrathi1611@gmail.com>
2021-08-09 23:46:35 +0530

    bugs fixes for insert

--- info.php
+++ info.php
@@ -32,16 +32,25 @@ if (isset($_GET['id'])){
 }
 
  ?>
-
  <div class="jumbotron jumbotron-fluid">
- <div class="bg-light p-2">
    <h2 class="text-center display-4"><?php echo $rating; ?></h2>
+   <!-- <img src="images/delhi 2.jpg" class="card-img-top" alt=""> -->
+ <div class="bg-light">
+   <div class="bg-image" style="
+         background-image: url('images/India_Gate.jpg');
+         height: 100%;
+
+          /* Center and scale the image nicely */
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+       ">
    <hr>
  <div class="row">
    <?php if ($flag == 1): while($row = $val->fetch_assoc()){?>
- <div class="col-sm-3">
+ <div class="col-sm-3 d-flex align-items-stretch">
  <div class="card">
-   <img src="images/<?php echo $row['image_name'];?>.jpg" height= "100px" class="card-img-top" alt="">
+   <img src="images/<?php echo $row['image_name'];?>.jpg" height= "150px" class="card-img-top" alt="">
    <div class="card-body">
      <h5 class="card-title"><?php echo $row["name"]; ?></h5>
      <p class="card-text"><?php echo $row['description']; ?></p>
@@ -54,6 +63,7 @@ if (isset($_GET['id'])){
 <?php } endif;?>
  </div>
  </div>
+  </div>
  </div>
 
  <?php include "footer.php" ?>
